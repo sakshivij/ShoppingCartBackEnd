f2d520c88ef46a097feaf80a55ac4aa434212376
sakshivij <sakshivij12@gmail.com>
2016-07-26 23:59:51 +0530

    Back End project

---
+++ src/main/java/com/niit/ShoppingCart/Model/Category.java
@@ -0,0 +1,10 @@
+package com.niit.ShoppingCart.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Category")
+public class Category {
+
+}
---
+++ src/main/java/com/niit/ShoppingCart/Model/Product.java
@@ -0,0 +1,75 @@
+package com.niit.ShoppingCart.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name = "Product")
+@Component
+public class Product {
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getCategoryID() {
+		return categoryID;
+	}
+	public void setCategoryID(String categoryID) {
+		this.categoryID = categoryID;
+	}
+	public String getSupplierID() {
+		return supplierID;
+	}
+	public void setSupplierID(String supplierID) {
+		this.supplierID = supplierID;
+	}
+	@Id
+	private String id;
+	private String name;
+	private double price;
+	private String brand;
+	private String color;
+	private String size;
+	@Column(name="category_id")
+	private String categoryID;
+	@Column(name="supplier_id")
+	private String supplierID;
+	
+
+}
